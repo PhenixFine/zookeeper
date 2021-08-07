f5ca232f8801e20eaae7deae7de0a3a880843cfc
Danny <64429863+PhenixFine@users.noreply.github.com>
2021-08-06 20:53:52 -0400

    Stage 2 of 4 for JetBrains Academy - Kotlin -  Zookeeper project. this stage just had us print a given ascii of a camel, so for some extra practice on reading files I saved the ascii to a file to read from.
    MainActivity.kt:
    • added a const to store the directory path of the ascii files
    • main() - calls importAnimal() get the camel file as a string.
    • importAnimal() - retrieves the file for the given animal name, returning the contents of the file as a string.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,20 @@
+import java.io.File
+import java.io.FileNotFoundException
+import java.lang.Exception
+
+private const val PATH = "animal_ascii_files/"
+
 fun main() {
-    println("I love animals!\n" +
-            "Let's check on the animals...\n" +
-            "The deer looks fine.\n" +
-            "The bat looks happy.\n" +
-            "The lion looks healthy.")
+    println(importAnimal("camel"))
+}
+
+fun importAnimal(animal: String): String {
+    return try {
+        File("$PATH$animal.txt").readLines().joinToString("\n")
+    } catch (e: Exception) {
+        when (e) {
+            is FileNotFoundException -> "$animal file could not be found"
+            else -> "There was an error in loading your file. Please ensure it is not open in another program.\n"
+        }
+    }
 }
